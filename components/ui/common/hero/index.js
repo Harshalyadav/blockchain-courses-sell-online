5d1c03002787e0467c37344b054766e93fb35bae
Harshal yadav <shailyadav3166@gmail.com>
2023-03-26 09:49:05 +0530

    nextjs, solidity and ethereum added

---
+++ components/ui/common/hero/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+export default function Hero() {
+
+  return (
+    <section className="lg:2/6 text-left my-28">
+      <div className="text-6xl font-semibold text-gray-900 leading-none">Sell your courses online</div>
+      <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">pay in cryptocurrency </div>
+      <div className="mt-5 sm:mt-8 flex lg:justify-start">
+      
+      </div>
+    </section>
+  )
+}
